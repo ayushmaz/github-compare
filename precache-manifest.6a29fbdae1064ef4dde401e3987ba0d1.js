f846b5c5ba174799f6f9104bf9619c5cc5ee4563
Ayush Majumder <majumdershuvu@gmail.com>
2020-09-13 21:58:12 +0530

    Updates

---
+++ precache-manifest.6a29fbdae1064ef4dde401e3987ba0d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ecb1641e3fa1522ec6dfcfed48d215",
+    "url": "/github-compare/index.html"
+  },
+  {
+    "revision": "69ff53bfa878cfb65bdb",
+    "url": "/github-compare/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b1bcd6365e0de55444ba",
+    "url": "/github-compare/static/js/2.71c0e3ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github-compare/static/js/2.71c0e3ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ff53bfa878cfb65bdb",
+    "url": "/github-compare/static/js/main.e9ea4af3.chunk.js"
+  },
+  {
+    "revision": "950215b11b35a9410c3f",
+    "url": "/github-compare/static/js/runtime-main.d49522ac.js"
+  }
+]);
